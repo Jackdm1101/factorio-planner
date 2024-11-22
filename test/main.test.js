0a28f70a281b38cf8a28fb3a9c9b0e64d9e8a2dc
Jackdm1101 <91748140+Jackdm1101@users.noreply.github.com>
2024-11-22 20:56:58 +0000

    Removed unnecessary braces from tests

--- test/main.test.js
+++ test/main.test.js
@@ -6,13 +6,13 @@ describe('ProductionChain', () => {
 
     describe('addProduct', () => {
         it('should not add a product that does not exist', () => {
-            expect(() => { chain.addProduct('invalid-product', 5) })
+            expect(() => chain.addProduct('invalid-product', 5))
                 .toThrow(`Product invalid-product not found`);
         });
 
         it.each(getTypesArrayExcept('string'))
             (`should throw an error if a productStr is: %p`, (productStrInput) => {
-                expect(() => { chain.addProduct(productStrInput, 5) })
+                expect(() => chain.addProduct(productStrInput, 5))
                     .toThrow('Unexpected type');
             });
     });
