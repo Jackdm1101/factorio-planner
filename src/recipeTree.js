eaa57c42481479e3d8a8019cdc3a0b3e8e7a16b3
Jackdm1101 <91748140+Jackdm1101@users.noreply.github.com>
2024-12-15 14:31:50 +0000

    Changed names from data* to node*

--- src/recipeTree.js
+++ src/recipeTree.js
@@ -1,7 +1,7 @@
 import recipes from './recipes.json' with { type: "json" };
 
 export class RecipeTree {
-    #data = []
+    #nodes = []
     static create(itemNameStr, itemsPerSec) {
         const tree = new RecipeTree;
         tree.#setup(itemNameStr, itemsPerSec);
@@ -9,33 +9,33 @@ export class RecipeTree {
     }
 
     #setup(itemNameStr, itemsPerSec) {
-        this.#data.push({item: itemNameStr, perSec: itemsPerSec, prev: [] });
-        const dataIndicesQueue = [0];
+        this.#nodes.push({itemName: itemNameStr, perSec: itemsPerSec, prev: [] });
+        const nodeIndicesQueue = [0];
 
-        while (dataIndicesQueue.length !== 0) {
-            const dataIndex = dataIndicesQueue.shift();
-            const data = this.#data[dataIndex];
-            const recipe = recipes[data.item];
+        while (nodeIndicesQueue.length !== 0) {
+            const nodeIndex = nodeIndicesQueue.shift();
+            const node = this.#nodes[nodeIndex];
+            const recipe = recipes[node.itemName];
 
             recipe.ingredients.forEach(ingredient => {
                 const result = recipe.results.find(
-                    result => result.name === data.item
+                    result => result.name === node.itemName
                 );
-                const newIndex = this.#data.length;
-                this.#data.push({
-                    item: ingredient.name,
-                    perSec: data.perSec * (ingredient.amount / result.amount),
+                const newNodeIndex = this.#nodes.length;
+                this.#nodes.push({
+                    itemName: ingredient.name,
+                    perSec: node.perSec * (ingredient.amount / result.amount),
                     prev: []
                 });
-                this.#data[dataIndex].prev.push(newIndex);
+                this.#nodes[nodeIndex].prev.push(newNodeIndex);
 
-                if (!recipes[ingredient.name]) return;
-                dataIndicesQueue.push(newIndex);
+                if (recipes[ingredient.name])
+                    nodeIndicesQueue.push(newNodeIndex);
             });
         }
     }
 
     getData() {
-        return this.#data;
+        return this.#nodes;
     }
 }
